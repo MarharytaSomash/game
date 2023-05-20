c9fea875d635bd7a34a9c5267e3833cb0db847b1
SomashMargarita <maargaritasivceva@gmail.come>
2023-05-20 13:23:25 +0300

    [dev] add responsive style for game page

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Question from '../components/Question';
 import MenuPyramid from '../components/MenuPyramid';
 import '../styles/gamePageStyle.scss';
 
 function GamePage() {
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+
   return (
-    <div className="game-page-wrapper">
-      <Question />
-      <MenuPyramid />
-    </div>
+    <>
+      {window.innerWidth <= 768 ? (
+        <div className={`game-page-wrapper ${isMenuOpen ? 'menu-open' : ''}`}>
+          {isMenuOpen ? <MenuPyramid /> : <Question />}
+        </div>
+      ) : (
+        <div className={`game-page-wrapper ${isMenuOpen ? 'menu-open' : ''}`}>
+          <Question />
+          <MenuPyramid />
+        </div>
+      )}
+      <button
+        className={`burger-menu ${isMenuOpen ? 'open' : ''}`}
+        onClick={() => setIsMenuOpen(!isMenuOpen)}>
+        <img
+          src={process.env.PUBLIC_URL + (isMenuOpen ? '/img/close.png' : '/img/open.png')}
+          alt="menu_icon"
+        />
+      </button>
+    </>
   );
 }
 
